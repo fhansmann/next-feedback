e9595a988226ec4483326ee0b4a04e65067677da
Florian Hansmann <hansmann.f@gmail.com>
2020-10-05 09:36:44 +0200

    Update

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
         <p className={styles.description}>
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
